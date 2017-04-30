b9789dabeef84745ba3c28a9aca9c7a95cd2644c
DAMONPC\Damon <damonluck@gmail.com>
2017-04-30 16:16:00 +0200

    Added serilog logging

---
+++ Infrastructure/DL.Infrastructure/AmbientLogger.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL.Infrastructure
+{
+    public static class AmbientLogger
+    {
+        public static void Setup()
+        {
+            SystemNotification = new SystemNotification();
+        }
+
+        public static ISystemNotification SystemNotification { get; private set; }
+    }
+}
---
+++ Infrastructure/DL.Infrastructure/SystemNotification.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+
+namespace DL.Infrastructure
+{
+    public interface ISystemNotification
+    {
+        ILogger Logger { get; }
+    }
+    internal class SystemNotification : ISystemNotification
+    {
+        public SystemNotification()
+        {
+            Log.Logger = new LoggerConfiguration().CreateLogger();
+            Logger = Log.Logger;
+        }
+
+        public ILogger Logger { get; }
+    }
+}
